559ec777acb58296ce940c77556ef5f318e7b677
Migacz85 <migacz85@gmail.com>
2019-04-09 23:35:06 +0100

    Add pagination to  get_bugs() and  get_bug_detail()
    Add in get_bug_detail(): toggling for upvoting
    Add custom variable title for templates
    Add @login_required

--- bugs/views.py
+++ bugs/views.py
@@ -1,48 +1,75 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Bugs, BugsUpvote, Comments
-
 from .forms import BugForm, CommentForm
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 
 def get_bugs(request):
-    """ Show all bugs """
+    """ Show list of all bugs """
 
     bugs = Bugs.objects.filter(
         published_date__lte=timezone.now()) .order_by('-published_date')
+    paginator = Paginator(bugs, 5)
+    page = request.GET.get('page')
+    bugs = paginator.get_page(page)
 
-    return render(request, "get_bugs.html", {'bugs': bugs})
+    return render(request, "get_bugs.html",
+                  {'bugs': bugs, }
+                  )
 
 
 def get_bug_detail(request, pk):
-    """ Show details about single bug
-    based on single post id """
+    """ Show details about single bug based on single post id
+    Show also all comments that are associated with single bug
+    """
     comments = Comments.objects.filter(ticket=pk).order_by('created_date')
+    paginator = Paginator(comments, 5)
+
+    page = request.GET.get('page')
+    comments = paginator.get_page(page)
+
+    bug = get_object_or_404(Bugs, pk=pk)
+    upvote = BugsUpvote.objects.filter(upvoted_bug=bug)
+
+    if str(upvote) == '<QuerySet []>':
+        upvoted = True
+    else:
+        upvoted = False
 
     bug = get_object_or_404(Bugs, pk=pk)
     bug.views += 1
     bug.save()
-    return render(request, "get_bug_detail.html", {'bug': bug, 'comments': comments})
+    return render(request, "get_bug_detail.html",
+                  {'bug': bug,
+                   'comments': comments,
+                   'upvoted': upvoted}
+                  )
 
 
+@login_required
 def create_or_edit_bug(request, pk=None):
     """ View that allows to create or edit bug """
     bug = get_object_or_404(Bugs, pk=pk) if pk else None
     if request.method == "POST":
         form = BugForm(request.POST, request.FILES, instance=bug)
-
         if form.is_valid():
             bug = form.save(commit=False)
             bug.username = request.user
             bug.created_date = timezone.now()
             bug = form.save()
-
             return redirect(get_bug_detail, bug.pk)
     else:
         form = BugForm(instance=bug)
-    return render(request, 'create_or_edit_bug.html', {'form': form})
+        title = 'Create or edit bug'
+        return render(
+            request, 'create_or_edit_bug.html',
+            {'form': form, 'title': title}
+        )
 
 
+@login_required
 def add_comment_bugs(request, pk=None):
     """Add comment to bugs"""
     bug = get_object_or_404(Bugs, pk=pk)
@@ -53,20 +80,22 @@ def add_comment_bugs(request, pk=None):
         instance.ticket = bug
         c.save()
         return redirect(get_bug_detail, bug.pk)
-    return render(request, "create_or_edit_bug.html", {'form': c})
+    title = "Add comment"
+    return render(request, "create_or_edit_bug.html", {'form': c, 'title': title})
 
 
+@login_required
 def upvote_bug(request, pk=None):
     """View is adding or taking one vote for each bug"""
 
     bug = get_object_or_404(Bugs, pk=pk)
     upvote = BugsUpvote.objects.filter(upvoted_bug=bug)
 
-    if str(upvote)=='<QuerySet []>':
+    if str(upvote) == '<QuerySet []>':
         try:
             u = get_object_or_404(
-            BugsUpvote, upvoted_bug=bug, user=request.user)
-        except:
+                BugsUpvote, upvoted_bug=bug, user=request.user)
+        except BaseException:
             u = BugsUpvote()
         bug.upvotes += 1
         bug.save()
@@ -78,7 +107,4 @@ def upvote_bug(request, pk=None):
         bug.save()
         upvote.delete()
 
-
-
     return redirect(get_bug_detail, bug.pk)
-    # TODO add user to db, then check if user already upvoted a bug
