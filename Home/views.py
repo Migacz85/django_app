79e0e2b1a8a9f9d0540674c5755cc7e04da515ab
Migacz85 <migacz85@gmail.com>
2019-05-09 20:51:47 +0100

    Add: Views to 500, and 404 error

--- Home/views.py
+++ Home/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from Home.forms import UserLoginForm, UserRegistrationForm
 from bugs.models import Issues
+from django.shortcuts import render_to_response
 
 
 def home(request):
@@ -111,3 +112,15 @@ def user_profile(request):
         'user_profile.html',
         {"profile": user}
     )
+
+
+def handler404(request, exception, template_name="404.html", ):
+    response = render_to_response("404.html")
+    response.status_code = 404
+    return response
+
+
+def handler500(request, exception, template_name="500.html"):
+    response = render_to_response("500.html")
+    response.status_code = 500
+    return response
